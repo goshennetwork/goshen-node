5dce69dd8d73aae6e560d593c5af092cf377f26e
sole12343 <m18640126583@163.com>
2023-04-23 17:20:48 +0800

    remove all function()

--- cmd/geth/apisignature.go
+++ cmd/geth/apisignature.go
@@ -4,10 +4,5 @@ import "github.com/ethereum/go-ethereum/console"
 
 func injectApiSignature(c *console.Console) {
 	c.Evaluate(`
-		if (typeof l2 !== 'undefined') {  
-			if (typeof l2.getBatch !== 'undefined') {  
-				l2.getBatch.toString = function() { return "function (batchIndex: uint64, useDetail: bool)" }
-			}
-		}
 	`)
 }
